0afd266725ad785e359f116a7f504175189f855a
Tyler Cheng <36546289+tchengsd@users.noreply.github.com>
2024-10-02 21:07:30 -0700

    Add files via upload

---
+++ basicMath.py
@@ -0,0 +1,14 @@
+def printMath():
+    w = 1.3
+    x = 9.5
+    y = 7
+    z = 12
+    print(type(w + x), w + x)
+    print(type(z - y), z - y)
+    print(type(w * y), w * y)
+
+def main():
+    printMath()
+
+if __name__ == '__main__':
+    main()
---
+++ introduction.py
@@ -0,0 +1,10 @@
+def printInfo():
+    print('My name is Tyler Cheng.')
+    print("I don't really have a favorite movie.")
+    print("My favorite food is sushi.")
+    
+def main():
+    printInfo()
+
+if __name__ == '__main__':
+    main()
